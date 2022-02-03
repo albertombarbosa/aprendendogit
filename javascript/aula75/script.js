a9c6a2335d48efebd7e606911da1993f8965b2fa
Alberto M Barbosa <albertobarbosa10@gmail.com>
2022-02-03 00:24:45 -0300

    iniciei e ja finalizei o exercício da aula 75

---
+++ javascript/aula75/script.js
@@ -0,0 +1,7 @@
+alert("Bem vindo! A seguir pediremos que informe alguns dados.");
+
+let nome = prompt("Informe o seu nome:");
+let idade = prompt("Informe a sua idade: ");
+let confimarcao = confirm("Você tem " +idade+ " anos de idade?");
+
+alert("Seu nome é " +nome+ " e a sua idade é " +idade+ "? " +confimarcao+ "!");
